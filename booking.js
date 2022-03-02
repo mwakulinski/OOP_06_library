feb2b1d936c1df2c2a72eeb5e235f457f0faeeb8
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-03-02 12:57:35 +0100

    update Booking

--- booking.js
+++ booking.js
@@ -6,11 +6,11 @@ const endOfDay = require("date-fns/endOfDay");
 const differenceInCalendarDays = require("date-fns/differenceInCalendarDays");
 
 class Booking {
-  constructor(user) {
+  constructor(user, books = []) {
     this.user = user;
     this.bookingDate = new Date();
     this.#setReturnDate();
-    this.booksList = [];
+    this.booksList = books;
     this.penalty = 0;
   }
 
@@ -23,13 +23,21 @@ class Booking {
     return this._user;
   }
 
-  //sprawdzić czy nie da się inaczej???
-  //date fns
+  set booksList(books) {
+    Validator.throwIfNotArr(books);
+    books.forEach((book) => Validator.throwIfNotProperInstacne(book, Book));
+    this._bookList = books;
+  }
+
+  get booksList() {
+    return this._bookList;
+  }
+
   #setReturnDate() {
     this.returnDate = endOfDay(add(new Date(), { days: 7 }));
   }
 
-  makeBooking(book) {
+  addBookToBooking(book) {
     Validator.throwIfNotProperInstacne(book, Book);
     this.booksList.push(book);
   }
@@ -60,6 +68,7 @@ class Booking {
   payPenalty(inputSum) {
     Validator.throwIfNotPositiveNumber(inputSum);
     this.penalty -= inputSum;
+    this.penalty = this.#roundToTwoDecimalPlaces(this.penalty);
   }
 
   findElementByIdInArr(arr, bookId) {
@@ -89,10 +98,11 @@ const book2 = new Book(
 );
 
 const booking = new Booking(user);
-booking.makeBooking(book1);
-booking.makeBooking(book2);
+booking.addBookToBooking(book1);
+booking.addBookToBooking(book2);
 booking.returnBook(book1.id);
 booking.returnBook(book2.id);
+booking.payPenalty(142);
 console.log(booking);
 // Booking dostaje użytkownika w constructorze
 
