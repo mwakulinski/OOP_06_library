119855969fc7068b4970272fa136da588d1a8ebe
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-03-03 09:03:29 +0100

    mian.js update

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-const LibraryBook = require("./library-book");
+const Book = require("./book");
 const Library = require("./library");
 
-const book1 = new LibraryBook(
+const book1 = new Book(
   "O psach",
   "Michal Wakulinski",
   "photo1",
   "Good book to reade about dogs"
 );
-const book2 = new LibraryBook(
+const book2 = new Book(
   "O kotach",
   "Barbara Nowak",
   "photo2",
   "Good book to reade about cats"
 );
-const book3 = new LibraryBook(
+const book3 = new Book(
   "O pszczołąch",
   "Maria Karaś",
   "photo3",
   "Good book to reade about bees"
 );
-const book4 = new LibraryBook(
+const book4 = new Book(
   "O dinozaurach",
   "Feliks Dzierżyński",
   "photo4",
@@ -41,13 +41,13 @@ library.bookBooks(library.usersList[0].id, [book3, book4]);
 library.bookBooks(library.usersList[1].id, [book3, book4]);
 library.bookBooks(library.usersList[2].id, [book3, book4]);
 
-library.returnBooks(library.usersList[0].id, book3.id);
-library.returnBooks(library.usersList[0].id, book4.id);
-library.returnBooks(library.usersList[1].id, book4.id);
-library.returnBooks(library.usersList[2].id, book4.id);
-library.deleteBook(library.booksList[0].id, 3);
+library.returnBook(library.usersList[0].id, book3.id);
+library.returnBook(library.usersList[0].id, book4.id);
+library.returnBook(library.usersList[1].id, book4.id);
+library.returnBook(library.usersList[2].id, book4.id);
+library.deleteBook(library.booksList[0].book.id, 2);
 
-library.deleteUser(library.usersList[0].id);
+// library.deleteUser(library.usersList[0].id);
 
 console.log(library);
 // console.log(library.availableBooks);
